27d7ec2304565fe10137d0869e969290002aa2d3
Kyrsten <ksurbayan@gmail.com>
2021-10-26 22:19:35 -0800

    intro screen

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 const express = require('express');
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -24,3 +25,22 @@ db.connect(err => {
         console.log(`Server running on port ${PORT}`);
     });
 });
+
+console.log(
+`
+ ______                 _                       
+|  ____|               | |                      
+| |__   _ __ ___  _ __ | | ___  _   _  ___  ___ 
+|  __| | '_ \` _ \\| '_  \| |/ _ \\| | | |/ _ \\/ _ \\
+| |____| | | | | | |_) | | (_) | |_| |  __/  __/
+|______|_| |_| |_| .__/|_|\\___/\\__,  |\\___|\\___|
+ _______         | | _          __/  |          
+|__   __|        |_|| |        |____/           
+   | |_ __ __ _  ___| | _____ _ __              
+   | | '__/ _\` |/ __| |/ / _ \\ '__|             
+   | | | | (_| | (__|   <  __/ |                
+   |_|_|  \\__,_|\\___|_|\\_\\___|_|                
+                                               
+                                               
+`
+);
