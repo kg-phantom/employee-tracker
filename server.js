fc81fdae6d205fbb0cd1bea084accd3e4aa0cb82
Kyrsten <ksurbayan@gmail.com>
2021-10-29 23:38:53 -0800

    add new employee

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const db = require('./db/connection');
 const express = require('express');
-const inquirer = require('inquirer');
-const Department = require('./lib/Department');
-const Role = require('./lib/Role');
-const Employee = require('./lib/Employee');
-
-// objects for tables
-const departmentTable = new Department();
-const roleTable = new Role();
-const employeeTable = new Employee();
+const promptUser = require('./utils/promptUser');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -47,41 +39,4 @@ console.log(
 `
 );
 
-const promptUser = () => {
-    inquirer
-    .prompt({
-        type: 'list',
-        name: 'action',
-        message: 'What would you like to do?',
-        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role']
-    })
-    .then(({ action }) => {
-        switch (action) {
-            case 'View All Departments':
-                departmentTable.displayDepartments();
-                break;
-            case 'View All Roles':
-                roleTable.displayRoles();
-                break;
-            case 'View All Employees':
-                employeeTable.displayEmployees();
-                break;
-            case 'Add A Department':
-                departmentTable.promptNewDepartment();
-                break;
-            case 'Add A Role':
-                roleTable.promptNewRole();
-                break;
-            case 'Add An Employee':
-                employeeTable.promptNewEmployee();
-                break;
-            case 'Update An Employee Role':
-                console.log(action);
-                break;
-        }
-    })
-    // .finally(() => {
-    //     setTimeout(promptUser, 500);
-    // });
-};
 promptUser();
---
+++ utils/promptUser.js
@@ -0,0 +1,46 @@
+const inquirer = require('inquirer');
+const Department = require('../lib/Department');
+const Role = require('../lib/Role');
+const Employee = require('../lib/Employee');
+
+// objects for tables
+const departmentTable = new Department();
+const roleTable = new Role();
+const employeeTable = new Employee();
+
+function promptUser() {
+    inquirer
+    .prompt({
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role']
+    })
+    .then(({ action }) => {
+        switch (action) {
+            case 'View All Departments':
+                departmentTable.displayDepartments();
+                break;
+            case 'View All Roles':
+                roleTable.displayRoles();
+                break;
+            case 'View All Employees':
+                employeeTable.displayEmployees();
+                break;
+            case 'Add A Department':
+                departmentTable.promptNewDepartment();
+                break;
+            case 'Add A Role':
+                roleTable.promptNewRole();
+                break;
+            case 'Add An Employee':
+                employeeTable.promptNewEmployee();
+                break;
+            case 'Update An Employee Role':
+                console.log(action);
+                break;
+        }
+    })
+};
+
+module.exports = promptUser;
